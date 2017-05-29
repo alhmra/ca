183e57cae4cfa12213c1b2dedc5748ea53852603
blukai <init1@protonmail.com>
2017-05-29 13:08:24 +0300

    add editorconfig, #1 1 ca for server & 1 ca for client

--- build.sh
+++ build.sh
@@ -3,49 +3,91 @@
 # Easy-rsa docs
 # https://openvpn.net/index.php/open-source/documentation/miscellaneous/77-rsa-key-management.html
 
-# $1 - ca dir
+set -e
 
-main() {
-	echo "Creating a new directory and prepares it to be used as a (CA) key management directory"
-	make-cadir $1
-	cd $1
-
-	echo "Applying variables"
-	cp ../vars.const vars
-	source vars > /dev/null
-	
-	echo "Cleaning 'keys' dir"
-	./clean-all
-	
-	# Build root certificate authority (CA) certificate/key
-	./build-ca
-	
-	./build-key-server $KEY_ORG
-	
-	# Build Diffie-Hellman parameters (necessary for the server end of a SSL/TLS connection)
-	./build-dh
-	
-	# create an "HMAC firewall" to help block DoS attacks and UDP port flooding.
-	openvpn --genkey --secret keys/ta.key
-}
-
-if [ -d "$1" ]
-then
-	read -p "The '$1' dir already exists. Do you wish to remove it and continue? [yes/no]: " RESP
-
-	if [[ ! $RESP =~ ^(y|Y|yes|Yes|n|N|no|No)$ ]]
-	then
-		echo "Incorrent response, exit"
-	else
-		if [[ $RESP =~ ^(y|Y|yes|Yes)$ ]]
-		then
-			rm -rf $1
-			main
+check_dir() {
+	if [ -d "$1" ]; then
+		read -p "The '$1' dir already exists. Do you wish to remove it and continue? [yes/no]: " RESP
+		if [[ $RESP =~ ^(y|Y|yes|Yes|n|N|no|No)$ ]]; then
+			if [[ $RESP =~ ^(y|Y|yes|Yes)$ ]]; then
+				rm -rf $1
+			else
+				echo "Exit"
+				exit 1
+			fi
 		else
-			echo "Exit"
+			echo "Incorrent response, exit"
 			exit 1
 		fi
 	fi
-else
-	main
-fi
+}
+
+server_ck() {
+	if [ -d "keys" ]; then
+		echo
+		echo "Server key"
+		echo
+		./build-key-server server
+
+		# Diffie-Hellman parameters, necessary for the server end of a SSL/TLS connection
+		./build-dh
+
+		# help block DoS attacks and UDP port flooding.
+		echo "Generating an 'HMAC firewall'"
+		openvpn --genkey --secret keys/ta.key
+	else
+		echo "'server_ck': whoops, wrong dir, exit"
+		exit 1
+	fi
+}
+
+client_ck() {
+	if [ -d "keys" ]; then
+		echo
+		echo "Client public key"
+		echo
+		./build-key public
+	else
+		echo "'server_ck': whoops, wrong dir, exit"
+		exit 1
+	fi
+}
+
+build_ca() {
+	if [ $1 ]; then
+		CA_DIR="ca-$1"
+
+		check_dir $CA_DIR
+
+		echo "Creating a new directory and prepares it to be used as a (CA) key management directory"
+		make-cadir $CA_DIR
+		cd $CA_DIR
+	
+		echo "Applying variables"
+		cp ../vars.const vars
+		source vars > /dev/null
+
+		echo "Cleaning 'keys' dir"
+		./clean-all
+
+		echo
+		echo "Building root certificate authority (CA) certificate/key for '$1'"
+		echo
+		./build-ca
+		
+		if [ "$1" = "server" ]; then
+			server_ck
+		elif [ "$1" = "client" ]; then
+			client_ck
+		fi
+
+		cd ..
+	else
+		echo "'build_ca' should have argument, exit"
+		exit 1
+	fi
+}
+
+# https://github.com/alhmra/ca/issues/1
+build_ca server
+build_ca client
