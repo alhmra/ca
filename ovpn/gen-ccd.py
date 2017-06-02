7489b887b0e7c6874381ce7bf57eb95341074e12
blukai <init1@protonmail.com>
2017-06-02 14:21:26 +0300

    Remove cached `ovpn/ccd` dir, some stage `prints` in `ovpn/gen-ccd.py`

--- ovpn/gen-ccd.py
+++ ovpn/gen-ccd.py
@@ -2,19 +2,22 @@ import os, ipaddress
 
 ASN_LIST_FILE = "asn-list.txt"
 DNS_LIST_FILE = "dns-list.txt"
-CCD_FILE = "ccd/DEFAULT"
+CCD_DIR = "ccd"
 
 def build_list_from_file(file):
+	print('Building list from', file)
+
 	list = []
 
 	for line in open(file, 'r'):
 		if not line.startswith(('#', '\n', '\r\n')):
-			asn = line.rstrip()
-			list.append(asn)
+			list.append(line.rstrip())
 	
 	return list
 
 def get_nets(asn_list):
+	print('Getting AS\'s nets..')
+
 	as_nets = []
 
 	for asn in asn_list:
@@ -22,43 +25,69 @@ def get_nets(asn_list):
 
 		as_nets.extend(whois.split())
 
+		print('ASN:', asn)
+
 	return as_nets
 
 def collapse_nets(nets):
+	print('Collapsing nets..')
+
 	list = [ipaddress.IPv4Network(addr) for addr in nets]
 	collapsed = ipaddress.collapse_addresses(list)
-
-	count = 0
+	
 	ranges = []
+
 	for addr in collapsed:
 		# Nets
 		# print(addr)
 
 		# Ranges
 		ranges.append(addr.with_netmask.replace('/', ' '))
-		count += 1
 
-	print('Networks before collapsing:', len(list))
-	print('Networks after collapsing:', count)
+	print('Networks before collapsing: %d, after collapsing: %d' % (
+		len(list),
+		len(ranges)
+	))
 
 	return ranges
 
-def ccd(file, list, nets):
-	rewrite = open(file, 'w')
-	append = open(file, 'a')
-	
-	for line in open(list, 'r'):
-		if not line.startswith(('#', '\n', '\r\n')):
-			ip = line.rstrip()
-			print('push "dhcp-option DNS ' + ip + '"\npush "route ' + ip + '"', file=rewrite)
-	
-	for net in nets:
-		print('push "route ' + net + '"', file=append)
+def gen_ccd(directory, dns_list, net_list):
+	print('Generating `ccd`')
+
+	if not os.path.exists(directory):
+		print('Directory `%s` does not exists, creating new one..' % directory)
+
+		os.makedirs(directory)
+
+	target = open(directory + "/DEFAULT", 'w')
+
+	for dns in dns_list:
+		print(
+			'push "dhcp-option DNS %s"' % dns,
+			'push "route %s"' % dns,
+			sep="\n",
+			file=target
+		)
+
+	for net in net_list:
+		print(
+			'push "route %s"' % net,
+			file=target
+		)
 
 def main():
-	list = build_list_from_file(ASN_LIST_FILE)
-	nets = get_nets(list)
+	abspath = os.path.abspath(__file__)
+	dname = os.path.dirname(abspath)
+	os.chdir(dname)
+	
+	asn_list = build_list_from_file(ASN_LIST_FILE)
+	dns_list = build_list_from_file(DNS_LIST_FILE)
+
+	nets = get_nets(asn_list)
 	collapsed = collapse_nets(nets)
-	ccd(CCD_FILE, DNS_LIST_FILE, collapsed)
+
+	gen_ccd(CCD_DIR, dns_list, collapsed)
+
+	print('Success!')
 
 main()
